0d2bc0f0df0b192bdd95c443d164561b3f9b6589
Antonio Paulino <tonnyarauujo@gmail.com>
2022-12-20 01:21:06 -0300

    rubocop lint

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
   describe 'validations' do
-    subject { described_class.new(name: 'John Doe', email: 'email@test.com', password: '12345678')}
+    subject { described_class.new(name: 'John Doe', email: 'email@test.com', password: '12345678') }
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
