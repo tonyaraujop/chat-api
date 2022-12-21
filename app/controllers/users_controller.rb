5fbca5246e95328cffb85c95efe81523b7868bf2
Antonio Paulino <tonnyarauujo@gmail.com>
2022-12-21 09:39:16 -0300

    remove error handling logic to creator use case

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,9 @@
 
 class UsersController < ApplicationController
   def sign_up
-    @user = Users::Creator.new.perform(**users_params)
+    @user = Users::Creator.new.perform(users_params)
+
     render :create, status: :created
-  rescue ArgumentError => e
-    raise MissingParameterError, e
   end
 
   private
